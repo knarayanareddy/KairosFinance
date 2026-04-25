1362627a3f15ff723077c00e1593c8e7c55d34a6
knarayanareddy <kirannreddyaero@gmail.com>
2026-04-25 11:41:40 +0200

    deploy: fix path in github pages

--- packages/presentation/vite.config.ts
+++ packages/presentation/vite.config.ts
@@ -1,5 +1,6 @@
 import path from "path";
 import { fileURLToPath } from "url";
+
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
@@ -8,6 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
+  base: './', // <-- Fixes the blank page on GitHub Pages!
   plugins: [react(), tailwindcss()],
   resolve: {
     dedupe: ['react', 'react-dom'],
